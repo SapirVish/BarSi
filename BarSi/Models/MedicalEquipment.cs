55fc464bbee8ddf7f22c98ec9604af0d29b5e8ed
Yairgadiel <Yairgadiel@gmail.com>
2020-06-20 22:00:39 +0300

    Changes in MedicalEquipment model: * Added a Price property * Added annotations

--- BarSi/Models/MedicalEquipment.cs
+++ BarSi/Models/MedicalEquipment.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace BarSi.Models
@@ -9,9 +11,17 @@ namespace BarSi.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
 
+        [Required]
+        [DataType(DataType.Currency)]
         public int Price { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9\s]*$", ErrorMessage = "Please enter positive value")]
+        public int Quantity { get; set; }
 
         public IEnumerable<MedicalEquipmentSupply> medicalEquipmentSupplies { get; set; }
     }
