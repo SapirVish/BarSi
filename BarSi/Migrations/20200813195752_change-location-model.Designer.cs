5e26d19ade40a296e1e4970ec192238ae23b9dab
gil <gil.by.1703@gmail.com>
2020-08-14 14:10:02 +0300

    update database

--- BarSi/Migrations/20200813195752_change-location-model.Designer.cs
+++ BarSi/Migrations/20200813195752_change-location-model.Designer.cs
@@ -4,14 +4,16 @@ using BarSi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BarSi.Migrations
 {
     [DbContext(typeof(BarSiContext))]
-    partial class BarSiContextModelSnapshot : ModelSnapshot
+    [Migration("20200813195752_change-location-model")]
+    partial class changelocationmodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BarSi/Migrations/20200813195752_change-location-model.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BarSi.Migrations
+{
+    public partial class changelocationmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Lng",
+                table: "Location",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Lat",
+                table: "Location",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Lng",
+                table: "Location",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Lat",
+                table: "Location",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
