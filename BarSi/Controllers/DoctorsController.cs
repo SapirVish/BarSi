bdc73ec82a8534b55152056506dffc312c53ab3c
Adi Cohen <cohennadi@gmail.com>
2020-09-09 19:42:18 +0300

    fix controller

--- BarSi/Controllers/DoctorsController.cs
+++ BarSi/Controllers/DoctorsController.cs
@@ -27,7 +27,7 @@ namespace BarSi.Controllers
             return View(await _context.Doctor.Include(d => d.City).Include(d => d.Hospital).ToListAsync());
         }
 
-        public async Task<IActionResult> Search(string first_name, string last_name, DateTime birthdate, int city, int hospital)
+        public async Task<IActionResult> Search(string first_name, string last_name, DateTime birthdate, string city, string hospital)
         {
             var doctors = _context.Doctor.AsQueryable();
             if (!String.IsNullOrEmpty(first_name))
@@ -36,10 +36,10 @@ namespace BarSi.Controllers
                 doctors = doctors.Where(d => d.LastName.ToLower().Contains(last_name.ToLower()));
             if (birthdate != DateTime.MinValue)
                 doctors = doctors.Where(d => d.Birthdate.Equals(birthdate));
-            if (hospital != 0)
-                doctors = doctors.Where(d => d.Hospital.Id == hospital);
-            if (city != 0)
-                doctors = doctors.Where(d => d.City.Id == city);
+            if (!String.IsNullOrEmpty(hospital))
+                doctors = doctors.Where(d => d.Hospital.Name.Contains(hospital));
+            if (!String.IsNullOrEmpty(city))
+                doctors = doctors.Where(d => d.City.Name.Contains(city));
 
 
             var doctors_results = await doctors.Include(d => d.City).Include(d => d.Hospital).ToListAsync();
